1137f86f409abec367ded76da78213e8db7cdcf7
Joshua Gold <killercatfish@gmail.com>
2017-12-05 09:20:36 -0500

    space invaders through part 3

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -23,7 +23,23 @@ class Sprite(turtle.Turtle):
     def move(self):
         self.fd(self.speed)
 
-
+        #Boundary detection
+        #check right side of border
+        if self.xcor() > 290:
+            self.setx(290)
+            self.rt(60)
+        #
+        if self.xcor() < -290:
+            self.setx(-290)
+            self.rt(60)
+        #
+        if self.ycor() > 290:
+            self.sety(290)
+            self.rt(60)
+        #
+        if self.ycor() < -290:
+            self.sety(-290)
+            self.rt(60)
 
 class Player(Sprite):
     def __init__(self, spriteshape, color, startx, starty):
@@ -42,6 +58,34 @@ class Player(Sprite):
 
     def decelerate(self):
         self.speed -= 1
+
+class Game():
+    def __init__(self):
+        self.level = 1
+        self.score = 0
+        self.state = "playing"
+        self.pen = turtle.Turtle()
+        self.lives = 3
+
+    def draw_border(self):
+        #Draw border
+        self.pen.speed(0)
+        self.pen.color("white")
+        self.pen.pensize(3)
+        self.pen.penup()
+        self.pen.goto(-300,300) #could make screen size a variable
+        self.pen.pendown()
+        for side in range(4):
+            self.pen.fd(600) #move forward 600 pixels
+            self.pen.rt(90) #turn right 90 degrees
+        self.pen.penup() #lift pen so no more drawing
+        self.pen.ht() #hide turtle
+
+#Create game object
+game = Game()
+
+#Draw the game border
+game.draw_border()
 
 #Create my sprites
 player = Player("triangle", "white", 0,0)
